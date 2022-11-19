2019092b8a76deff2f0aa06accc3c64122744b13
perejro <perejro@centro.team>
2022-11-19 10:20:40 +0200

    change host

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
   try {
-    $pdo = new PDO("pgsql:host=postgres;port=54320;dbname=test-data-base;user=root;password=root123");
+    $pdo = new PDO("pgsql:host=localhost;port=54320;dbname=test-data-base;user=root;password=root123");
     var_dump('Success connection');
   } catch (PDOException $err) {
     echo "<pre>";
