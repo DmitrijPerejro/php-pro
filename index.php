27a290cc0660c5f9ab95f112136fc5135c10ce97
perejro <perejro@centro.team>
2022-11-19 02:32:15 +0200

    code

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
   try {
-    $pdo = new PDO("pgsql:host=localhost;port=54320;dbname=test-data-base;user=root;password=root123;");
+    $pdo = new PDO("pgsql:host=postgres;port=54320;dbname=test-data-base;user=root;password=root123");
     var_dump('Success connection');
   } catch (PDOException $err) {
-    var_dump($err);
+    echo "<pre>";
+    print_r($err);
+    echo "</pre>";
   }
