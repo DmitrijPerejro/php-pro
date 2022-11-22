abc029fe1387d0b840e06e97e3e8b143246e551f
perejro <perejro@centro.team>
2022-11-22 12:47:54 +0200

    docs

--- core/Color.php
+++ core/Color.php
@@ -3,12 +3,18 @@
   
   class Color
   {
+
     private int $red;
     private int $green;
     private int $blue;
     private static int $minColorValue = 0;
     private static int $maxColorValue = 255;
-    
+  
+    /**
+     * @param int $r
+     * @param int $g
+     * @param int $b
+     */
     public function __construct(int $r, int $g, int $b)
     {
       $this->red = $r;
@@ -66,21 +72,36 @@
       $this->rangeGuard($b);
       $this->blue = $b;
     }
-    
+  
+    /**
+     * @param int $color
+     * @return void
+     * @throws \Exception
+     */
     private function rangeGuard(int $color): void {
       if ($color < self::$minColorValue || $color > self::$maxColorValue) {
         throw new \Exception("Color $color is out of range");
       }
     }
-    
+  
+    /**
+     * @return Color
+     */
     public static function random(): Color {
       return new Color(self::makeRandom(), self::makeRandom(), self::makeRandom());
     }
-    
+  
+    /**
+     * @return int
+     */
     private static function makeRandom(): int {
       return rand(self::$minColorValue, self::$maxColorValue);
     }
-    
+  
+    /**
+     * @param Color $incomingColor
+     * @return Color
+     */
     public function mix(Color $incomingColor): Color {
       return new Color(
         ($this->getRed() + $incomingColor->getRed()) / 2,
@@ -88,17 +109,31 @@
         ($this->getBlue() + $incomingColor->getBlue()) / 2,
       );
     }
-    
+  
+    /**
+     * @param int $r
+     * @param int $g
+     * @param int $b
+     * @return void
+     */
     public function setValues(int $r, int $g, int $b): void {
       $this->setRed($r);
       $this->setGreen($g);
       $this->setBlue($b);
     }
-    
+  
+    /**
+     * @param Color $incomingColor
+     * @return bool
+     */
     public function equals(Color $incomingColor): bool {
       return $this == $incomingColor;
     }
   
+    /**
+     * @param Color $incomingColor
+     * @return bool
+     */
     public function strictEquals(Color $incomingColor): bool {
       return $this === $incomingColor;
     }
