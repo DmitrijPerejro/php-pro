94110ad81a07cc99f438fbae1cd581a85830eb1b
perejro <perejro@centro.team>
2022-11-27 22:19:16 +0200

    code

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
   require_once __DIR__ . '/vendor/autoload.php';
   
-  use Core\User;
-  use Exceptions\Error;
-  
-  try {
-    $user = new User('Perejro', 10, 'a@gmail.com');
-    $user->setEmail('s@gmail.com');
-  } catch (Error $error) {
-    $error->print();
-  }
+  $test = new \Core\Test();
+  $res = $test->getSum();
+  var_dump($res);
   
