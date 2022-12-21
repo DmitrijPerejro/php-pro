a157b28d9c4746191c775990143cc9c5b415c9ed
perejro <perejro@centro.team>
2022-12-21 18:23:02 +0200

    ccode

--- index.php
+++ index.php
@@ -5,6 +5,27 @@
   use Fabric\EconomTaxiCreator;
   use Fabric\StandartTaxiCreator;
   use Fabric\LuxTaxiCreator;
+  use Fabric\Creator;
+  use Taxi\Taxi;
   
-  $e = new EconomTaxiCreator();
+  function getClientTaxi($type): Taxi {
+    try {
+      $creator = match ($type){
+        'lux' => new LuxTaxiCreator(),
+        'standart' => new StandartTaxiCreator(),
+        'econom' => new EconomTaxiCreator(),
+      };
+    } catch (\UnhandledMatchError $err) {
+      throw new Exception("type $type doesn't support");
+    }
+    
+    return $creator->getTaxi();
+  }
   
+  try {
+    $taxi = getClientTaxi('econom');
+    var_dump($taxi->getModel());
+    var_dump($taxi->getPrice());
+  } catch (\Exception $err) {
+    echo $err->getMessage();
+  }
